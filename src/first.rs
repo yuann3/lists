a19b63bfa75ba74f70e1f196534b9aa7419e8a2a
Yiyuan Li <yy@eyuan.me>
2025-05-25 00:08:26 +0800

    autocommit

--- src/first.rs
+++ src/first.rs
@@ -1,13 +1,30 @@
+use std::mem;
+
 pub struct List {
     head: Link,
 }
 
 struct Node {
     elem: i32,
-    next: List,
+    next: Link,
 }
 
 enum Link {
     Empty,
     More(Box<Node>),
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new_node);
+    }
 }
